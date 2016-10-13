8cb1a3c165e5639e3dc21391e846182b1964e3c5
vagit <gracefulgirl207@gmail.com>
2016-10-13 20:36:46 +0700

    milestone 2

--- ls.js
+++ ls.js
@@ -1,10 +1,24 @@
 require('./helper')
+let fs = require('fs').promise
+let path = require('path')
+let {dir} = require('yargs')
+    .default('dir', __dirname)
+    .argv
 
 async function ls() {
-  // Use 'await' inside 'async function's
-  console.log('Executing ls function...')
+    // Use 'await' inside 'async function's
+    console.log('Executing ls function...')
+    let promise = fs.readdir(__dirname)
+    let fileNames = await fs.readdir(dir)
+    // Your implementation here
 
-  // Your implementation here
+    for (let fileName of fileNames) {
+        let filePath = path.join(__dirname, fileName)
+        // TODO: Obtain the stat promise from fs.stat(filePath)
+        console.log(filePath)
+        // TODO: Use stat.isDirectory to exclude subdirectories
+        // ...
+    }
 }
 
 ls()
